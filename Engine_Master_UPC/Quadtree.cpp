88cdacf9410a24511b5c827e817cfa1cceaf2a94
Miquel Barbarà <miquel.barbara.garrido@gmail.com>
2026-01-28 17:26:55 +0100

    Initial proposal for Quadtree: Creation, Insert, Subdivide.

--- Engine_Master_UPC/Quadtree.cpp
+++ Engine_Master_UPC/Quadtree.cpp
@@ -1,1 +1,74 @@
 #include "Quadtree.h"
+
+Quadtree::Quadtree(const DirectX::SimpleMath::Rectangle& area, UINT depth): bounds(area), depth(depth)
+{
+}
+
+void Quadtree::insert(GameObject& gameObject)
+{
+	if (isLeaf()) 
+	{
+		if (objects.size() >= MAX_OBJECTS) 
+		{
+			subdivide();
+			insertToChildren(gameObject);
+		}
+		else 
+		{
+			objects.push_back(&gameObject);
+		}
+	} 
+	else 
+	{
+		insertToChildren(gameObject);
+	}
+}
+
+
+void Quadtree::subdivide()
+{
+	if (depth >= MAX_DEPTH) return;
+
+	float halfWidth = bounds.width / 2.0f;
+	float halfHeight = bounds.height / 2.0f;
+
+	children[0] = new Quadtree(DirectX::SimpleMath::Rectangle(bounds.x, bounds.y, halfWidth, halfHeight), depth + 1); // Top-left
+	children[1] = new Quadtree(DirectX::SimpleMath::Rectangle(bounds.x + halfWidth, bounds.y, halfWidth, halfHeight), depth + 1); // Top-right
+	children[2] = new Quadtree(DirectX::SimpleMath::Rectangle(bounds.x, bounds.y + halfHeight, halfWidth, halfHeight), depth + 1); // Bottom-left
+	children[3] = new Quadtree(DirectX::SimpleMath::Rectangle(bounds.x + halfWidth, bounds.y + halfHeight, halfWidth, halfHeight), depth + 1); // Bottom-right
+
+	for (auto& object : objects) {
+		insert(*object);
+	}
+	objects.clear();
+}
+
+
+bool Quadtree::isInside(Quadtree& child, Vector3& position)
+{
+	if (child.bounds.Contains(position.x, position.y)) 
+	{
+		return true;
+	}
+
+	return false;
+}
+
+void Quadtree::insertToChildren(GameObject& gameObject)
+{
+	auto transform = gameObject.getTransform();
+
+	for (auto& child : children)
+	{
+		if (isInside(*child, transform.getPosition()))
+		{
+			child->insert(gameObject);
+			return;
+		}
+	}
+}
+
+std::vector<GameObject*> Quadtree::GetObjects(BoundingFrustum& frustum) const
+{
+	return std::vector<GameObject*>();
+}
--- Engine_Master_UPC/Quadtree.h
+++ Engine_Master_UPC/Quadtree.h
@@ -1,8 +1,35 @@
 #pragma once
+#include "SimpleMath.h"
+#include "GameObject.h"
+
+using namespace DirectX::SimpleMath;
+using namespace DirectX;
+
+enum Quadrant {
+	TOP_LEFT = 0,
+	TOP_RIGHT = 1,
+	BOTTOM_LEFT = 2,
+	BOTTOM_RIGHT = 3
+};
 
 class Quadtree {
 public:
+	static const int MAX_OBJECTS = 1;
+	static const int MAX_DEPTH = 5;
 
+	Quadtree(const DirectX::SimpleMath::Rectangle& area, UINT depth = 0);
+	void insert(GameObject& gameObject);
 private:
+	void subdivide();
+	bool isLeaf() const { return children[TOP_LEFT] == nullptr; }
+	bool isInside(Quadtree& child, Vector3& position);
+	void insertToChildren(GameObject& gameObject);
 
+	std::vector<GameObject*> GetObjects(BoundingFrustum& frustum) const;
+
+	DirectX::SimpleMath::Rectangle bounds;
+	std::vector<GameObject*> objects;
+	UINT depth = 0;
+
+	Quadtree* children[4] = { nullptr, nullptr, nullptr, nullptr };
 };
