4f158c4f96aab391c3c89de98e138d2516476914
Miquel Barbarà <miquel.barbara.garrido@gmail.com>
2026-02-01 17:52:26 +0100

    Added: ·Move and remove functions. ·Unordered map to stablish relationship between GameObject and Quadtree. ·Intersects functions between Frustum and BoundingBox/Rectangle
    Some small style corrections

--- Engine_Master_UPC/Quadtree.cpp
+++ Engine_Master_UPC/Quadtree.cpp
@@ -1,21 +1,24 @@
-#include "Quadtree.h"
+﻿#include "Quadtree.h"
 
-Quadtree::Quadtree(const DirectX::SimpleMath::Rectangle& area, UINT depth): bounds(area), depth(depth)
+
+Quadtree::Quadtree(const DirectX::SimpleMath::Rectangle& area, UINT depth): m_bounds(area), m_depth(depth)
 {
+
 }
 
 void Quadtree::insert(GameObject& gameObject)
 {
 	if (isLeaf()) 
 	{
-		if (objects.size() >= MAX_OBJECTS) 
+		if (m_objects.size() >= MAX_OBJECTS) 
 		{
 			subdivide();
 			insertToChildren(gameObject);
 		}
 		else 
 		{
-			objects.push_back(&gameObject);
+			m_objectLocationMap[&gameObject] = this;
+			m_objects.push_back(&gameObject);
 		}
 	} 
 	else 
@@ -24,29 +27,48 @@ void Quadtree::insert(GameObject& gameObject)
 	}
 }
 
-
-void Quadtree::subdivide()
+void Quadtree::move(GameObject& movedObjects)
 {
-	if (depth >= MAX_DEPTH) return;
+	remove(movedObjects);
+	insert(movedObjects);
+}
 
-	float halfWidth = bounds.width / 2.0f;
-	float halfHeight = bounds.height / 2.0f;
-
-	children[0] = new Quadtree(DirectX::SimpleMath::Rectangle(bounds.x, bounds.y, halfWidth, halfHeight), depth + 1); // Top-left
-	children[1] = new Quadtree(DirectX::SimpleMath::Rectangle(bounds.x + halfWidth, bounds.y, halfWidth, halfHeight), depth + 1); // Top-right
-	children[2] = new Quadtree(DirectX::SimpleMath::Rectangle(bounds.x, bounds.y + halfHeight, halfWidth, halfHeight), depth + 1); // Bottom-left
-	children[3] = new Quadtree(DirectX::SimpleMath::Rectangle(bounds.x + halfWidth, bounds.y + halfHeight, halfWidth, halfHeight), depth + 1); // Bottom-right
-
-	for (auto& object : objects) {
-		insert(*object);
+void Quadtree::remove(GameObject& gameObject)
+{
+	Quadtree* location = m_objectLocationMap[&gameObject];
+	if (location) 
+	{
+		auto& objs = location->m_objects;
+		objs.erase(std::remove(objs.begin(), objs.end(), &gameObject), objs.end());
+		m_objectLocationMap.erase(&gameObject);
 	}
-	objects.clear();
 }
 
 
-bool Quadtree::isInside(Quadtree& child, Vector3& position)
+void Quadtree::subdivide()
 {
-	if (child.bounds.Contains(position.x, position.y)) 
+	if (m_depth >= MAX_DEPTH) return;
+
+	float halfWidth = m_bounds.width / 2.0f;
+	float halfHeight = m_bounds.height / 2.0f;
+
+	m_children[0] = new Quadtree(DirectX::SimpleMath::Rectangle(m_bounds.x, m_bounds.y, halfWidth, halfHeight), m_depth + 1); // Top-left
+	m_children[1] = new Quadtree(DirectX::SimpleMath::Rectangle(m_bounds.x + halfWidth, m_bounds.y, halfWidth, halfHeight), m_depth + 1); // Top-right
+	m_children[2] = new Quadtree(DirectX::SimpleMath::Rectangle(m_bounds.x, m_bounds.y + halfHeight, halfWidth, halfHeight), m_depth + 1); // Bottom-left
+	m_children[3] = new Quadtree(DirectX::SimpleMath::Rectangle(m_bounds.x + halfWidth, m_bounds.y + halfHeight, halfWidth, halfHeight), m_depth + 1); // Bottom-right
+
+	for (auto& object : m_objects)
+	{
+		insert(*object);
+	}
+
+	m_objects.clear();
+}
+
+
+bool Quadtree::isInside(Quadtree& quad, Vector3& position)
+{
+	if (quad.m_bounds.Contains(position.x, position.y))
 	{
 		return true;
 	}
@@ -58,7 +80,7 @@ void Quadtree::insertToChildren(GameObject& gameObject)
 {
 	auto transform = gameObject.getTransform();
 
-	for (auto& child : children)
+	for (Quadtree* child : m_children)
 	{
 		if (isInside(*child, transform.getPosition()))
 		{
@@ -68,7 +90,108 @@ void Quadtree::insertToChildren(GameObject& gameObject)
 	}
 }
 
-std::vector<GameObject*> Quadtree::GetObjects(BoundingFrustum& frustum) const
+
+Plane* Quadtree::getFrustumPlanes(const BoundingFrustum& frustum) const
 {
-	return std::vector<GameObject*>();
+	XMVECTOR planes[PLANE_COUNT];
+	frustum.GetPlanes(&planes[NEAR_PLANE], &planes[FAR_PLANE], &planes[RIGHT_PLANE], &planes[LEFT_PLANE], &planes[TOP_PLANE], &planes[BOTTOM_PLANE]);
+
+	static Plane result[PLANE_COUNT] =
+	{
+		Plane(planes[NEAR_PLANE]),
+		Plane(planes[FAR_PLANE]),
+		Plane(planes[RIGHT_PLANE]),
+		Plane(planes[LEFT_PLANE]),
+		Plane(planes[TOP_PLANE]),
+		Plane(planes[BOTTOM_PLANE])
+	};
+
+	return result;
 }
+
+bool Quadtree::intersects(const Plane* planes, const Vector3& center, const Vector3& extents) const 
+{
+	Vector3 axisX = Vector3::UnitX;
+	Vector3 axisY = Vector3::UnitY;
+	Vector3 axisZ = Vector3::UnitZ;
+
+	for (int i = 0; i < PLANE_COUNT; i++)
+	{
+		Vector3 normal = planes[i].Normal();
+		float distance = planes[i].DotCoordinate(center);
+
+		float radius =
+			fabsf(normal.Dot(axisX)) * extents.x +
+			fabsf(normal.Dot(axisY)) * extents.y +
+			fabsf(normal.Dot(axisZ)) * extents.z;
+
+		if (distance + radius < 0.0f)
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+bool Quadtree::intersects(const BoundingFrustum& frustum, const BoundingBox& box) const
+{
+	Plane* planes = getFrustumPlanes(frustum);
+	const Vector3& center = box.Center;
+	const Vector3& extents = box.Extents;
+	return intersects(planes, center, extents);
+}
+
+bool Quadtree::intersects(const BoundingFrustum& frustum, const DirectX::SimpleMath::Rectangle& rectangle, int minY = -1000, int maxY = 1000) const
+{
+	Plane* planes = getFrustumPlanes(frustum);
+
+	Vector3 center;
+	center.x = rectangle.x + rectangle.width * 0.5f;
+	center.z = rectangle.y + rectangle.height * 0.5f;
+	center.y = (minY + maxY) * 0.5f;
+
+	Vector3 extents;
+	extents.x = rectangle.width * 0.5f;
+	extents.z = rectangle.height * 0.5f;
+	extents.y = (maxY - minY) * 0.5f;
+
+	return intersects(planes, center, extents);
+}
+
+
+std::vector<GameObject*> Quadtree::getObjects(BoundingFrustum& frustum) const
+{
+	std::vector<GameObject*> result;
+	gatherObjects(frustum, result);
+	return result;
+}
+
+void Quadtree::gatherObjects(BoundingFrustum& frustum, std::vector<GameObject*>& outObjects) const
+{
+	if (!intersects(frustum, m_bounds)) 
+	{
+		return;
+	}
+
+	if (isLeaf())
+	{
+		for (GameObject* obj : m_objects)
+		{
+			//PLACEHOLDER
+			BoundingBox box = BoundingBox();
+			if (intersects(frustum, box))
+				outObjects.push_back(obj);
+		}
+		return;
+	}
+
+	for (const Quadtree* child : m_children)
+	{
+		if (child) 
+		{
+			child->gatherObjects(frustum, outObjects);
+		}
+	}
+}
+
--- Engine_Master_UPC/Quadtree.h
+++ Engine_Master_UPC/Quadtree.h
@@ -5,11 +5,25 @@
 using namespace DirectX::SimpleMath;
 using namespace DirectX;
 
+enum FrustrumPlane {
+	NEAR_PLANE = 0,
+	FAR_PLANE = 1,
+	RIGHT_PLANE = 2,
+	LEFT_PLANE = 3,
+	TOP_PLANE = 4,
+	BOTTOM_PLANE = 5,
+
+	PLANE_COUNT = 6,
+};
+
+
 enum Quadrant {
 	TOP_LEFT = 0,
 	TOP_RIGHT = 1,
 	BOTTOM_LEFT = 2,
-	BOTTOM_RIGHT = 3
+	BOTTOM_RIGHT = 3,
+
+	QUADRANT_COUNT = 4,
 };
 
 class Quadtree {
@@ -19,17 +33,29 @@ public:
 
 	Quadtree(const DirectX::SimpleMath::Rectangle& area, UINT depth = 0);
 	void insert(GameObject& gameObject);
+	void move(GameObject& movedObjects);
+	void remove(GameObject& gameObject);
+
+	std::vector<GameObject*> getObjects(BoundingFrustum& frustum) const;
 private:
 	void subdivide();
-	bool isLeaf() const { return children[TOP_LEFT] == nullptr; }
-	bool isInside(Quadtree& child, Vector3& position);
+	bool isLeaf() const { return m_children[TOP_LEFT] == nullptr; }
+	bool isInside(Quadtree& quad, Vector3& position);
 	void insertToChildren(GameObject& gameObject);
 
-	std::vector<GameObject*> GetObjects(BoundingFrustum& frustum) const;
+	Plane* getFrustumPlanes(const BoundingFrustum& frustum) const;
 
-	DirectX::SimpleMath::Rectangle bounds;
-	std::vector<GameObject*> objects;
-	UINT depth = 0;
+	bool intersects(const Plane* planes, const Vector3& center, const Vector3& extents) const;
+	bool intersects(const BoundingFrustum& frustum, const BoundingBox& box) const;
+	bool intersects(const BoundingFrustum& frustum, const DirectX::SimpleMath::Rectangle& rectangle, int minY = -1000, int maxY = 1000) const;
 
-	Quadtree* children[4] = { nullptr, nullptr, nullptr, nullptr };
+	void gatherObjects(BoundingFrustum& frustum,  std::vector<GameObject*>& outObjects) const;
+
+	DirectX::SimpleMath::Rectangle m_bounds;
+	std::vector<GameObject*> m_objects;
+	UINT m_depth = 0;
+
+	Quadtree* m_children[QUADRANT_COUNT] = { nullptr, nullptr, nullptr, nullptr };
+
+	std::unordered_map<GameObject*, Quadtree*> m_objectLocationMap;
 };
