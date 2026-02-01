71c67c688120fcf509db49b16c228aa6e3e4ab10
Raúl Rodríguez Trotonda <raul.rodriguez.trotonda@gmail.com>
2026-02-01 14:31:46 +0100

    Class GameObject and class Component initialized
    Both GameObject and Component classes initialized

--- Engine_Master_UPC/Component.h
+++ Engine_Master_UPC/Component.h
@@ -1,10 +1,23 @@
 #pragma once
 
 class GameObject;
+enum ComponentType
+{
+    TRANSFORM
+};
 
 class Component {
 public:
     virtual ~Component() = default;
 
+    const short getID() { return m_uuid; }
+    const ComponentType getType() { return m_type; }
+
+    virtual void drawUi();
+
+protected:
+    ComponentType m_type;
+
 private:
+    short m_uuid;
 };
