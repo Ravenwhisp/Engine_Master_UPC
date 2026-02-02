9752e4a58cbfb78a7ba378726d80fa8e8b98655d
Raúl Rodríguez Trotonda <74973674+Ermys-Code@users.noreply.github.com>
2026-02-02 18:32:22 +0100

    GameObject and Component classes (#1)
    * Class GameObject and class Component initialized
    
    Both GameObject and Component classes initialized
    
    * Add GameObject reference to the Component class
    
    * Fix code style errors

--- Engine_Master_UPC/GameObject.cpp
+++ Engine_Master_UPC/GameObject.cpp
@@ -1,1 +1,34 @@
-#include "GameObject.h"
+ #include "GameObject.h"
+
+GameObject::GameObject(short newUuid) : m_uuid(newUuid)
+{
+
+}
+
+GameObject::~GameObject()
+{
+}
+
+bool GameObject::AddComponent(Component* newComponent)
+{
+    if (newComponent->getType() == TRANSFORM)
+    {
+        return false;
+    }
+
+    m_components.push_back(newComponent);
+    return true;
+}
+
+bool GameObject::RemoveComponent(Component* componentToRemove)
+{
+    m_components.erase(std::remove_if(m_components.begin(), m_components.end(), [componentToRemove](Component* component)
+        {
+            if (component == componentToRemove)
+            {
+                delete component;
+                return true;
+            }
+            return false;
+        }), m_components.end());
+}
--- Engine_Master_UPC/GameObject.h
+++ Engine_Master_UPC/GameObject.h
@@ -1,14 +1,44 @@
 #pragma once
 #include "Transform.h"
 #include "Component.h"
+#include <vector>
+
+class Scene;
+enum Layer;
+enum Tag;
 
 class GameObject {
 public:
-	GameObject();
+	GameObject(short newUuid);
+	~GameObject();
 
-	Transform& getTransform() { return transform; }
-	const Transform& getTransform() const { return transform; }
+	const short GetID() { return m_uuid; }
+	const char* GetName() { return m_name; }
+	const bool GetActive() { return m_active; }
+	const bool GetStatic() { return m_isStatic; }
+	const Layer GetLayer() { return m_layer; }
+	const Tag GetTag() { return m_tag; }
+	Scene* GetScene() { return m_scene; }
+	GameObject* GetParent() { return m_parent; }
+	Transform* GetTransform() { return m_transform; }
+
+	void SetName(char* newName) { m_name = newName; }
+	void SetActive(bool newActive) { m_active = newActive; }
+	void SetStatic(bool newIsStatic) { m_isStatic = newIsStatic; }
+	void SetLayer(Layer newLayer) { m_layer = newLayer; }
+	void SetTag(Tag newTag) { m_tag = newTag; }
+	bool AddComponent(Component* newComponent);
+	bool RemoveComponent(Component* componentToRemove);
 
 private:
-	Transform transform;
+	short m_uuid;
+	char* m_name;
+	bool m_active;
+	bool m_isStatic;
+	Layer m_layer;
+	Tag m_tag;
+	Scene* m_scene;
+	GameObject* m_parent;
+	Transform* m_transform;
+	std::vector<Component*> m_components;
 };
