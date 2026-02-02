9d97b335907aeebe15521537a259f3e99b142b99
Raúl Rodríguez Trotonda <74973674+Ermys-Code@users.noreply.github.com>
2026-02-02 18:32:22 +0100

    GameObject and Component classes (#1)
    * Class GameObject and class Component initialized
    
    Both GameObject and Component classes initialized
    
    * Add GameObject reference to the Component class
    
    * Fix code style errors

--- Engine_Master_UPC/Component.h
+++ Engine_Master_UPC/Component.h
@@ -1,10 +1,24 @@
 #pragma once
 
 class GameObject;
+enum ComponentType
+{
+    TRANSFORM
+};
 
 class Component {
 public:
     virtual ~Component() = default;
 
+    const short getID() { return m_uuid; }
+    const ComponentType getType() { return m_type; }
+
+    virtual void drawUi();
+
+protected:
+    ComponentType m_type;
+    GameObject* m_gameObject;
+
 private:
+    short m_uuid;
 };
