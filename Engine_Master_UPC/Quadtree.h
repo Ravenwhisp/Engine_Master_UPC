8e696bb5efc40253da020c59db8f5c6b68873a58
Miquel Barbarà <miquel.barbara.garrido@gmail.com>
2026-02-13 10:25:53 +0100

    Merging with engine/setup

--- Engine_Master_UPC/Quadtree.h
+++ Engine_Master_UPC/Quadtree.h
@@ -5,27 +5,6 @@
 using namespace DirectX::SimpleMath;
 using namespace DirectX;
 
-enum FrustrumPlane {
-	NEAR_PLANE = 0,
-	FAR_PLANE = 1,
-	RIGHT_PLANE = 2,
-	LEFT_PLANE = 3,
-	TOP_PLANE = 4,
-	BOTTOM_PLANE = 5,
-
-	PLANE_COUNT = 6,
-};
-
-
-enum Quadrant {
-	TOP_LEFT = 0,
-	TOP_RIGHT = 1,
-	BOTTOM_LEFT = 2,
-	BOTTOM_RIGHT = 3,
-
-	QUADRANT_COUNT = 4,
-};
-
 class Quadtree {
 public:
 	static const int MAX_OBJECTS = 1;
@@ -39,8 +18,6 @@ public:
 
 	std::vector<GameObject*> getObjects(BoundingFrustum& frustum) const;
 	std::vector<RectangleData> getQuadrants() const;
-
-	std::vector<GameObject*> getObjects(BoundingFrustum& frustum) const;
 private:
 	friend class QuadNode;
 
