11fedeaf3fa0c857919ee1d5f0f44d0f38eca553
Jordi Pi <pimoreno.j@outlook.es>
2026-02-16 14:37:52 +0100

    Skybox rootSig & pipeline and shaders created

--- Engine_Master_UPC/ResourcesModule.h
+++ Engine_Master_UPC/ResourcesModule.h
@@ -7,6 +7,7 @@
 #include "DescriptorsModule.h"
 #include <DepthBuffer.h>
 #include <RenderTexture.h>
+#include <DirectXTex.h>
 
 using namespace std::filesystem;
 
@@ -47,6 +48,7 @@ public:
 	std::unique_ptr<DepthBuffer>	createDepthBuffer(float windowWidth, float windowHeight);
 	std::unique_ptr<Texture>		createTexture2DFromFile(const path& filePath, const char* name);
 	std::unique_ptr<Texture>		createNullTexture2D();
+	std::unique_ptr<Texture>		createTextureCubeFromFile(const path& filePath, const char* name);
 	std::unique_ptr<RenderTexture>	createRenderTexture(float windowWidth, float windowHeight);
 	RingBuffer*						createRingBuffer(size_t size);
 	VertexBuffer*					createVertexBuffer(const void* data, size_t numVertices, size_t vertexStride);
@@ -58,4 +60,9 @@ private:
 	ComPtr<ID3D12Device4>			m_device;
 	CommandQueue*					m_queue;
 	std::vector<DefferedResource>	m_defferedResources;
+
+	void generateMipmapsIfMissing(ScratchImage& image, TexMetadata& metaData);
+	void buildSubresourceData(const ScratchImage& image, const TexMetadata& metaData, std::vector<D3D12_SUBRESOURCE_DATA>& subData);
+	void uploadTextureAndTransition(ID3D12Resource* dstTexture, const std::vector<D3D12_SUBRESOURCE_DATA>& subData);
+
 };
