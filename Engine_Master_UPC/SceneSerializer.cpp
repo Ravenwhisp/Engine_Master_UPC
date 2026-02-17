2fdefc3ff9c384190b5149893fb131c4e54029ba
Héctor Núñez Carpio <accounts.hector@nunezcarpio.com>
2026-02-17 19:53:48 +0100

    Create default folder if not exist on constructor

--- Engine_Master_UPC/SceneSerializer.cpp
+++ Engine_Master_UPC/SceneSerializer.cpp
@@ -11,8 +11,19 @@ constexpr std::string_view SCENE_FOLDER = "Assets/Scenes/";
 
 SceneSerializer::SceneSerializer()
 {
-
+    if (!std::filesystem::exists(SCENE_FOLDER))
+    {
+        if (std::filesystem::create_directories(SCENE_FOLDER))
+        {
+            LOG("Created scene folder: %s\n", SCENE_FOLDER.data());
+        }
+        else
+        {
+            LOG("Failed to create scene folder: %s\n", SCENE_FOLDER.data());
+        }
+    }
 }
+
 
 SceneSerializer::~SceneSerializer()
 {
