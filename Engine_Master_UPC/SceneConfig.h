aabff17f3e63fa2b18b9456971df675b246fb73f
Jordi Pi <pimoreno.j@outlook.es>
2026-02-20 19:05:12 +0100

    Added ambient light settings in scene
    # Conflicts:
    #	Engine_Master_UPC/TextureImporter.cpp

--- Engine_Master_UPC/SceneConfig.h
+++ Engine_Master_UPC/SceneConfig.h
@@ -9,17 +9,20 @@ class SceneConfig : public EditorWindow
 private:
 	SceneModule* m_sceneModule;
 
-private:
-	std::string m_sceneName;
-	GameObject* m_camera = nullptr;
-
-    void drawSkyboxSettings();
-    bool m_skyboxDirty = false;
-
 public:
     SceneConfig();
     ~SceneConfig() = default;
 
     const char* getWindowName() const override { return "Scene Configuration"; }
     void render() override;
+
+private:
+    void drawSkyboxSettings();
+    void drawLightSettings();
+
+    std::string m_sceneName;
+    GameObject* m_camera = nullptr;
+
+    bool m_skyboxDirty = false;
+    bool m_lightingDirty = false;
 };
