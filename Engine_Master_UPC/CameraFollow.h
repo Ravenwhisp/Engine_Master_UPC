75f6981bb5c35a307b867d9f4e8b6b962204d1ee
Jordi Pi <pimoreno.j@outlook.es>
2026-02-26 12:46:28 +0100

    Camera with zoom when players separate

--- Engine_Master_UPC/CameraFollow.h
+++ Engine_Master_UPC/CameraFollow.h
@@ -14,7 +14,16 @@ public:
 
 private:
 	void setFollowTargets();
+
+	Vector3 computeFollowPoint() const;
+	float computeTargetExtraHeight(const Vector3& p1, const Vector3& p2) const;
+	float smoothExtraHeight(float current, float target, float sharpness, float dt) const;
+	Vector3 computeDesiredCameraPosition(const Vector3& followPoint) const;
+	Vector3 smoothCameraPosition(const Vector3& current, const Vector3& target, float sharpness, float dt) const;
+
+
 	Vector3 lerpVector(const Vector3& start, const Vector3& end, float alpha) const;
+	float lerpFloat(float start, float end, float alpha) const;
 
 	UID m_firstTargetUid = 0;
 	UID m_secondTargetUid = 0;
@@ -24,5 +33,11 @@ private:
 	Vector3 m_transformOffset = Vector3::Zero;
 	Vector3 m_rotationOffset = Vector3::Zero;
 
+	float m_zoomStartDistance = 0.0;
+	float m_zoomEndDistance = 0.0;
+	float m_maxExtraHeight = 0.0;
+	float m_currentExtraHeight = 0.0;
+
 	float m_followSharpness = 12.0f;
+	float m_zoomSharpness = 8.0f;
 };
