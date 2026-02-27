37b99b28dc51b3f8054cfcdd96a357c4072fd798
David Varo <david.vama21@gmail.com>
2026-02-27 23:10:35 +0100

    Added move operation (with errText for now for debugging)

--- Engine_Master_UPC/FileSystemModule.h
+++ Engine_Master_UPC/FileSystemModule.h
@@ -47,6 +47,7 @@ public:
 	unsigned int save(const char* filePath, const void* buffer, unsigned int size, bool append = false) const;
 
 	bool copy(const char* sourceFilePath, const char* destinationFilePath) const;
+	bool move(const char* sourceFilePath, const char* destinationFilePath) const;
 	bool deleteFile(const char* filePath) const;
 	bool createDirectory(const char* directoryPath) const;
 	bool exists(const char* filePath) const;
