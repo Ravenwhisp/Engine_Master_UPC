770980e94a0da6a22fbcf1df3bef095413d38a83
David Varo <david.vama21@gmail.com>
2026-02-27 23:11:45 +0100

    Initial move files on UI implementations (not working yet)

--- Engine_Master_UPC/FileDialog.h
+++ Engine_Master_UPC/FileDialog.h
@@ -3,6 +3,12 @@
 #include <filesystem>
 #include "FileSystemModule.h"
 
+enum Command {
+    NONE,
+    MOVE,
+    COPY
+};
+
 class FileDialog : public EditorWindow {
 public:
     FileDialog();
@@ -13,6 +19,11 @@ private:
     void drawDirectoryTree(const std::shared_ptr<FileEntry> entry);
     void drawAssetGrid(const std::shared_ptr<FileEntry> directory);
 
+    bool moveFile(FileEntry* targetDirectory);
+
     std::filesystem::path m_currentDirectory;
     std::shared_ptr<FileEntry> m_selectedItem;
+
+    Command m_lastActionRequested = Command::NONE;
+    std::filesystem::path m_fileToManage;
 };
