590e6aea0daf01eebc4b2250b99d6d4bb9c86d65
Miquel Barbarà <miquel.barbara.garrido@gmail.com>
2026-02-28 13:52:58 +0100

    Improved readability in FileDialog

--- Engine_Master_UPC/FileDialog.h
+++ Engine_Master_UPC/FileDialog.h
@@ -11,7 +11,6 @@ enum Command {
 
 class FileDialog : public EditorWindow {
 public:
-    FileDialog();
     void render() override;
     const char* getWindowName() const override { return "FileDialog"; }
 
@@ -19,7 +18,17 @@ private:
     void drawDirectoryTree(const std::shared_ptr<FileEntry> entry);
     void drawAssetGrid(const std::shared_ptr<FileEntry> directory);
 
+    // Actions
     bool moveFile(FileEntry* targetDirectory);
+    void createNewFolder();
+    void pasteFile(const std::shared_ptr<FileEntry>& directory);
+    void importAsset(const std::shared_ptr<FileEntry>& asset);
+    void cutItem(const std::shared_ptr<FileEntry>& asset);
+    void deleteFolder(const std::shared_ptr<FileEntry>& asset);
+
+    // Navigation
+    void navigateTo(const std::filesystem::path& path);
+    void handleAssetDoubleClick(const std::shared_ptr<FileEntry>& asset);
 
     std::filesystem::path m_currentDirectory;
     std::shared_ptr<FileEntry> m_selectedItem;
