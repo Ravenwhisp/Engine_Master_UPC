90605740726e5ba9d9f78eb9a8f8adc3f83b4fce
Jordi Pi <pimoreno.j@outlook.es>
2026-03-04 12:31:36 +0100

    UI Anchor points

--- Engine_Master_UPC/Transform2D.h
+++ Engine_Master_UPC/Transform2D.h
@@ -13,16 +13,23 @@ class Transform2D : public Component
 public:
     Transform2D(UID id, GameObject* owner);
 
-    Float2 position{ 0.0f, 0.0f }; 
-    Float2 size{ 100.0f, 100.0f };
-    Float2 pivot{ 0.5f, 0.5f };
-
     Rect2D getRect() const;
+    Rect2D getRect(const Rect2D& parent) const;
 
     void drawUi() override;
 
     rapidjson::Value getJSON(rapidjson::Document& domTree) override;
     bool deserializeJSON(const rapidjson::Value& componentValue) override;
 
+public:
+    Float2 position{ 0.0f, 0.0f };
+    Float2 size{ 100.0f, 100.0f };
+    Float2 pivot{ 0.5f, 0.5f };
+
+    Float2 anchorMin{ 0.0f, 0.0f };
+    Float2 anchorMax{ 0.0f, 0.0f };
 private:
+    void drawAnchorPresetsUI();
+    void setPointPreset(float ax, float ay, float px, float py);
+
 };
