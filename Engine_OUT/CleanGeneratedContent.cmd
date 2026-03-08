6920e01d812b4a59b295d78136891719f1081447
henukk <accounts.hector@nunezcarpio.com>
2026-03-08 21:12:19 +0100

    fix cleaner script, avoid removing metadata

--- Engine_OUT/CleanGeneratedContent.cmd
+++ Engine_OUT/CleanGeneratedContent.cmd
@@ -7,13 +7,5 @@ echo Eliminando carpeta Library...
 rmdir /s /q "Library"
 )
 
-REM Borrar archivos .metadata dentro de Assets recursivamente
-if exist "Assets" (
-echo Eliminando archivos .metadata en Assets...
-for /r "Assets" %%f in (*.metadata) do (
-del /q "%%f"
-)
-)
-
 echo Limpieza completada.
 pause
