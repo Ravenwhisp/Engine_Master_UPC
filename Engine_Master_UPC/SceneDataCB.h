a8a89cdc44ce0a73ddfaca5e02675e1eff5829dc
Héctor Núñez Carpio <accounts.hector@nunezcarpio.com>
2026-03-16 16:00:49 +0100

    Scene class created

---
+++ Engine_Master_UPC/SceneDataCB.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "SimpleMath.h"
+
+struct SceneDataCB
+{
+	DirectX::SimpleMath::Vector3 viewPos;
+	float pad0 = 0.0f;
+};
---
+++ Engine_Master_UPC/SkyBoxSettings.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "UID.h"
+
+struct SkyBoxSettings
+{
+	bool enabled = true;
+	UID cubemapAssetId = 0;
+};
