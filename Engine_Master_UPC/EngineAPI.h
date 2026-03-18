b05e66cda5ae193fdafda16f316024611c838d44
Jordi Pi <pimoreno.j@outlook.es>
2026-03-18 15:28:38 +0100

    new api functions

--- Engine_Master_UPC/EngineAPI.h
+++ Engine_Master_UPC/EngineAPI.h
@@ -8,6 +8,7 @@
 
 #include "ScriptTypes.h"
 #include "ComponentType.h"
+#include "Tag.h"
 #include "SimpleMath.h"
 
 using DirectX::SimpleMath::Vector3;
@@ -32,10 +33,15 @@ namespace GameObjectAPI
 {
     ENGINE_API Transform* getTransform(GameObject* gameObject);
     ENGINE_API const Transform* getTransform(const GameObject* gameObject);
+
     ENGINE_API bool isActiveSelf(const GameObject* gameObject);
     ENGINE_API bool isActiveInHierarchy(const GameObject* gameObject);
     ENGINE_API void setActive(GameObject* gameObject, bool active);
+
     ENGINE_API const char* getName(const GameObject* gameObject);
+
+    ENGINE_API Tag getTag(const GameObject* gameObject);
+    ENGINE_API void setTag(GameObject* gameObject, Tag tag);
 }
 
 namespace TransformAPI
@@ -43,14 +49,25 @@ namespace TransformAPI
     ENGINE_API Vector3 getPosition(const Transform* transform);
     ENGINE_API void setPosition(Transform* transform, const Vector3& newPosition);
 
+    ENGINE_API Vector3 getScale(const Transform* transform);
+    ENGINE_API void setScale(Transform* transform, const Vector3& newScale);
+
     ENGINE_API Vector3 getEulerDegrees(const Transform* transform);
     ENGINE_API void setRotationEuler(Transform* transform, const Vector3& eulerDegrees);
+
+    ENGINE_API Vector3 getForward(const Transform* transform);
+    ENGINE_API Vector3 getRight(const Transform* transform);
+    ENGINE_API Vector3 getUp(const Transform* transform);
+    ENGINE_API void translate(Transform* transform, const Vector3& delta);
 }
 
 namespace ComponentAPI
 {
     ENGINE_API GameObject* getOwner(Component* component);
     ENGINE_API const GameObject* getOwner(const Component* component);
+
+    ENGINE_API bool isActive(const Component* component);
+    ENGINE_API void setActive(Component* component, bool active);
 }
 
 namespace Scene
@@ -60,4 +77,6 @@ namespace Scene
 
     ENGINE_API GameObject* getDefaultCameraGameObject();
     ENGINE_API void setDefaultCameraByGameObject(GameObject* gameObject);
+
+    ENGINE_API GameObject* findGameObjectByTag(Tag tag, bool onlyActive = true);
 }
