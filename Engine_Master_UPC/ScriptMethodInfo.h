e9ce40799f445b905792bdd29cec28ee27eda991
kevinql190 <136074257+kevinql190@users.noreply.github.com>
2026-04-12 17:09:34 +0200

    Feature/UI buttons improvement (#84)
    * improve button inspector ui
    
    * add Expose Parameters from Script methods on Button events
    
    * button component states change texture
    
    * filters
    
    * move filters
    
    ---------
    
    Co-authored-by: Jordi Pi <pimoreno.j@outlook.es>

--- Engine_Master_UPC/ScriptMethodInfo.h
+++ Engine_Master_UPC/ScriptMethodInfo.h
@@ -2,8 +2,24 @@
 
 class Script;
 
+enum class ScriptMethodParamType
+{
+    None,
+    Float,
+    Int,
+    Bool,
+    Vec3,
+    String,
+    Unsupported
+};
+
+using ScriptMethodParamFunc = void(*)(Script*, const void*);
+
 struct ScriptMethodInfo
 {
     const char* name;
     void(*func)(Script*);
+    ScriptMethodParamType paramType = ScriptMethodParamType::None;
+    const char* paramName = nullptr;
+    ScriptMethodParamFunc paramFunc = nullptr;
 };
