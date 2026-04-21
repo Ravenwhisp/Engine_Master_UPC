517767671d39fa1cc79a2c409e8e25e8c7dbbdab
Jordi Pi <pimoreno.j@outlook.es>
2026-04-21 13:21:30 +0200

    Small improvement to SERIALIZED macros

--- Engine_Master_UPC/ScriptFieldMacros.h
+++ Engine_Master_UPC/ScriptFieldMacros.h
@@ -11,6 +11,7 @@
 #define IMPLEMENT_SCRIPT_FIELDS(ScriptType, ...)                              \
     ScriptFieldList ScriptType::getExposedFields() const                      \
     {                                                                         \
+        using ThisScript = ScriptType;                                        \
         static const ScriptFieldInfo ownFields[] = { __VA_ARGS__ };           \
         return ScriptFieldList(ownFields, SCRIPT_FIELD_COUNT(ownFields));     \
     }
@@ -19,6 +20,7 @@
 #define IMPLEMENT_SCRIPT_FIELDS_INHERITED(ScriptType, BaseType, ...)          \
     ScriptFieldList ScriptType::getExposedFields() const                      \
     {                                                                         \
+        using ThisScript = ScriptType;                                        \
         ScriptFieldList exposedFields = BaseType::getExposedFields();         \
         static const ScriptFieldInfo ownFields[] = { __VA_ARGS__ };           \
         exposedFields.append(ownFields, SCRIPT_FIELD_COUNT(ownFields));       \
@@ -28,23 +30,23 @@
 
 // Field helper macros
 
-#define SERIALIZED_FLOAT(ScriptType, MemberName, DisplayName, Min, Max, Speed) \
-    { DisplayName, ScriptFieldType::Float, offsetof(ScriptType, MemberName), { Min, Max, Speed } }
+#define SERIALIZED_FLOAT(MemberName, DisplayName, Min, Max, Speed) \
+    { DisplayName, ScriptFieldType::Float, offsetof(ThisScript, MemberName), { Min, Max, Speed } }
 
-#define SERIALIZED_INT(ScriptType, MemberName, DisplayName) \
-    { DisplayName, ScriptFieldType::Int, offsetof(ScriptType, MemberName) }
+#define SERIALIZED_INT(MemberName, DisplayName) \
+    { DisplayName, ScriptFieldType::Int, offsetof(ThisScript, MemberName) }
 
-#define SERIALIZED_BOOL(ScriptType, MemberName, DisplayName) \
-    { DisplayName, ScriptFieldType::Bool, offsetof(ScriptType, MemberName) }
+#define SERIALIZED_BOOL(MemberName, DisplayName) \
+    { DisplayName, ScriptFieldType::Bool, offsetof(ThisScript, MemberName) }
 
-#define SERIALIZED_VEC3(ScriptType, MemberName, DisplayName) \
-    { DisplayName, ScriptFieldType::Vec3, offsetof(ScriptType, MemberName) }
+#define SERIALIZED_VEC3(MemberName, DisplayName) \
+    { DisplayName, ScriptFieldType::Vec3, offsetof(ThisScript, MemberName) }
 
-#define SERIALIZED_STRING(ScriptType, MemberName, DisplayName) \
-    { DisplayName, ScriptFieldType::String, offsetof(ScriptType, MemberName) }
+#define SERIALIZED_STRING(MemberName, DisplayName) \
+    { DisplayName, ScriptFieldType::String, offsetof(ThisScript, MemberName) }
 
-#define SERIALIZED_ENUM_INT(ScriptType, MemberName, DisplayName, NamesArray, Count) \
-    { DisplayName, ScriptFieldType::EnumInt, offsetof(ScriptType, MemberName), {}, { NamesArray, Count } }
+#define SERIALIZED_ENUM_INT(MemberName, DisplayName, NamesArray, Count) \
+    { DisplayName, ScriptFieldType::EnumInt, offsetof(ThisScript, MemberName), {}, { NamesArray, Count } }
 
-#define SERIALIZED_COMPONENT_REF(ScriptType, MemberName, DisplayName, ComponentTypeValue) \
-    { DisplayName, ScriptFieldType::ComponentRef, offsetof(ScriptType, MemberName), {}, {}, { ComponentTypeValue } }
+#define SERIALIZED_COMPONENT_REF(MemberName, DisplayName, ComponentTypeValue) \
+    { DisplayName, ScriptFieldType::ComponentRef, offsetof(ThisScript, MemberName), {}, {}, { ComponentTypeValue } }
