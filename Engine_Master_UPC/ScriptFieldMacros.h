98b50ab000aa6d758dbea4fb32e74c54cfd25bd4
Jordi Pi <pimoreno.j@outlook.es>
2026-04-21 12:43:26 +0200

    New macro for each fieldType for script commodity

--- Engine_Master_UPC/ScriptFieldMacros.h
+++ Engine_Master_UPC/ScriptFieldMacros.h
@@ -24,3 +24,27 @@
         exposedFields.append(ownFields, SCRIPT_FIELD_COUNT(ownFields));       \
         return exposedFields;                                                 \
     }
+
+
+// Field helper macros
+
+#define SERIALIZED_FLOAT(ScriptType, MemberName, DisplayName, Min, Max, Speed) \
+    { DisplayName, ScriptFieldType::Float, offsetof(ScriptType, MemberName), { Min, Max, Speed } }
+
+#define SERIALIZED_INT(ScriptType, MemberName, DisplayName) \
+    { DisplayName, ScriptFieldType::Int, offsetof(ScriptType, MemberName) }
+
+#define SERIALIZED_BOOL(ScriptType, MemberName, DisplayName) \
+    { DisplayName, ScriptFieldType::Bool, offsetof(ScriptType, MemberName) }
+
+#define SERIALIZED_VEC3(ScriptType, MemberName, DisplayName) \
+    { DisplayName, ScriptFieldType::Vec3, offsetof(ScriptType, MemberName) }
+
+#define SERIALIZED_STRING(ScriptType, MemberName, DisplayName) \
+    { DisplayName, ScriptFieldType::String, offsetof(ScriptType, MemberName) }
+
+#define SERIALIZED_ENUM_INT(ScriptType, MemberName, DisplayName, NamesArray, Count) \
+    { DisplayName, ScriptFieldType::EnumInt, offsetof(ScriptType, MemberName), {}, { NamesArray, Count } }
+
+#define SERIALIZED_COMPONENT_REF(ScriptType, MemberName, DisplayName, ComponentTypeValue) \
+    { DisplayName, ScriptFieldType::ComponentRef, offsetof(ScriptType, MemberName), {}, {}, { ComponentTypeValue } }
