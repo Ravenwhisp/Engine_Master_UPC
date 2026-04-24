a9bf6d3f80af2517908e932574d5a577f7f44c9b
Jordi Pi <pimoreno.j@outlook.es>
2026-04-24 17:41:17 +0200

    Pairs created to be able to detect OnTriggerEnter and OnTriggerExit

--- Engine_Master_UPC/ModuleTrigger.h
+++ Engine_Master_UPC/ModuleTrigger.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Module.h"
+#include "UID.h"
 
 #include <vector>
 
@@ -16,12 +17,46 @@ public:
     void unregisterTrigger(TriggerComponent* trigger);
 
 private:
-    void detectOverlaps();
+    struct TriggerOverlap
+    {
+        UID a = 0;
+        UID b = 0;
 
+        TriggerOverlap() = default;
+
+        TriggerOverlap(UID first, UID second)
+        {
+            if (first < second)
+            {
+                a = first;
+                b = second;
+            }
+            else
+            {
+                a = second;
+                b = first;
+            }
+        }
+
+        bool operator==(const TriggerOverlap& other) const
+        {
+            return a == other.a && b == other.b;
+        }
+    };
+
+private:
+    void detectOverlaps();
+    void processOverlapChanges();
+
+    void removeOverlaps(UID triggerId);
     bool isTriggerRegistered(TriggerComponent* trigger) const;
     bool isValidTrigger(TriggerComponent* trigger) const;
     bool intersectsAABB(TriggerComponent* a, TriggerComponent* b);
+    bool containsOverlap(const std::vector<TriggerOverlap>& overlaps, const TriggerOverlap& overlap) const;
 
 private:
     std::vector<TriggerComponent*> m_triggers;
+
+    std::vector<TriggerOverlap> m_previousOverlaps;
+    std::vector<TriggerOverlap> m_currentOverlaps;
 };
