5d82e6934eb8049979010d3f5642c7062e1fe0a5
Jordi <pimoreno.j@outlook.es>
2026-04-25 12:57:59 +0200

    TriggerComponent cleanup and pragma regions

--- Engine_Master_UPC/TriggerComponent.h
+++ Engine_Master_UPC/TriggerComponent.h
@@ -24,6 +24,7 @@ public:
 
     std::unique_ptr<Component> clone(GameObject* newOwner) const override;
 
+#pragma region Shape data
     TriggerShape getShape() const { return m_shape; };
 
     const Vector3& getCenter() const { return m_center; };
@@ -31,16 +32,26 @@ public:
 
     void setCenter(const Vector3& center) ;
     void setSize(const Vector3& size);
+#pragma endregion
 
+#pragma region Bounds
     Engine::BoundingBox& getWorldBox();
-    Engine::BoundingBox& getWorldAABB();
+    Engine::BoundingBox& getWorldAABB();  
+#pragma endregion
+
 
 private:
+
+#pragma region Bounds calculation
     void recalculateWorldBounds();
     bool isValidSize() const;
-    bool fitToModelBounds();
+#pragma endregion
+
+#pragma region Defaul Model bounds
+    bool setDefaultBoundsFromModel();
     void includeHierarchyModelBounds(GameObject* object, const Matrix& triggerWorldInverse, Vector3& boundsMin, Vector3& boundsMax,  bool& hasBounds);
     void includeMeshRendererBounds(MeshRenderer* meshRenderer, const Matrix& triggerWorldInverse, Vector3& boundsMin, Vector3& boundsMax, bool& hasBounds);
+#pragma endregion
 
 private:
     TriggerShape m_shape = TriggerShape::Box;
@@ -50,11 +61,11 @@ private:
 
     Engine::BoundingBox m_worldBox;
     Engine::BoundingBox m_worldAABB;
+    bool m_boundsDirty = true;
+
 
     TriggerDebugDrawMode m_debugDrawMode = TriggerDebugDrawMode::RotatedBox;
 
-    bool m_boundsDirty = true;
-
-    bool m_autoFitOnInit = true;
+    bool m_setDefaultBoundsOnInit = true;
 
 };
