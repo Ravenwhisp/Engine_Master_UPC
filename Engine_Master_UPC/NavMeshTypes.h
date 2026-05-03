d762c6a97b55f5db3ba22a7440b0376f3784e666
mialskywalker <53373335+mialskywalker@users.noreply.github.com>
2026-05-03 09:50:31 +0200

    Add NavMesh type enums
    Add NavMeshTypes.h defining enum classes for navigation

---
+++ Engine_Master_UPC/NavMeshTypes.h
@@ -0,0 +1,19 @@
+enum class NavAreaType
+{
+	Default,
+	Spectral,
+	Blocked
+};
+
+enum class NavPolyFlags
+{
+	Default,
+	Spectral
+};
+
+enum class NavAgentProfile
+{
+	PlayerNormal,
+	PlayerSpectral,
+	EnemyGround
+};
