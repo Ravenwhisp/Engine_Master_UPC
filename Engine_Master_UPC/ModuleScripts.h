12f020f8ab0ede05e4922d181e120f2fe3502524
Jordi Pi <pimoreno.j@outlook.es>
2026-05-11 13:31:59 +0200

    ModuleScripts divided in different pragma regions to keep it organized

--- Engine_Master_UPC/ModuleScripts.h
+++ Engine_Master_UPC/ModuleScripts.h
@@ -25,18 +25,22 @@ public:
     bool init() override;
     bool cleanUp() override;
 
+#pragma region ScriptRuntime
     bool buildAndReloadGameScriptsDll();
 
     void instantiateSceneScripts();
     void destroySceneScripts();
 
+    bool isGameScriptsLoaded() const { return m_gameScriptsModule != nullptr; }
+#pragma endregion
+
+#pragma region BuildSettings
     bool loadScriptBuildSettings();
     bool saveScriptBuildSettings() const;
 
     ScriptBuildSettings& getScriptBuildSettings() { return m_buildSettings; }
     const ScriptBuildSettings& getScriptBuildSettings() const { return m_buildSettings; }
-
-    bool isGameScriptsLoaded() const { return m_gameScriptsModule != nullptr; }
+#pragma endregion
 
 private: 
     struct ScriptReloadInfo
@@ -47,35 +51,45 @@ private:
     };
 
 private:
+#pragma region DllLoading
     bool loadGameScriptsDll();
     bool unloadGameScriptsDll();
-    bool buildGameScriptsProject();
-
-    std::filesystem::path resolveBuildPath(const std::string& path) const;
 
     unsigned int getNextReloadVersion();
     std::string buildRuntimeDllPath(unsigned int version) const;
     std::string buildRuntimePdbPath(unsigned int version) const;
     bool copyFileToRuntimePath(const std::string& sourcePath, const std::string& runtimePath);
+#pragma endregion
 
+#pragma region ScriptBuild
+    bool buildGameScriptsProject();
+    std::filesystem::path resolveBuildPath(const std::string& path) const;
+#pragma endregion
+
+#pragma region RuntimeCleanup
     bool isRuntimeScriptFile(const std::filesystem::directory_entry& entry) const;
     void cleanRuntimeScriptFiles();
+#pragma endregion
 
+#pragma region ScriptReloadState
     std::vector<ScriptReloadInfo> saveSceneScriptReloadInfo();
     void restoreSceneScriptReloadInfo(std::vector<ScriptReloadInfo>& reloadInfos);
+#pragma endregion
 
 private:
-    HMODULE m_gameScriptsModule = nullptr;
-
+#pragma region Constants
     static constexpr const char* SCRIPT_DLL_NAME = "GameScripts.dll";
     static constexpr const char* RUNTIME_DLL_PREFIX = "GameScripts_runtime_";
     static constexpr const char* SCRIPT_PDB_NAME = "GameScripts.pdb";
     static constexpr const char* SCRIPT_BUILD_CONFIGURATION = "Debug";
     static constexpr const char* SCRIPT_BUILD_PLATFORM = "x64";
     static constexpr const char* SCRIPT_BUILD_SETTINGS_FILE = "script_build_settings.ini";
+#pragma endregion
+
+    HMODULE m_gameScriptsModule = nullptr;
+    std::string m_loadedDllPath;
+    unsigned int m_reloadVersion = 0;
 
     ScriptBuildSettings m_buildSettings;
 
-    std::string m_loadedDllPath;
-    unsigned int m_reloadVersion = 0;
 };
