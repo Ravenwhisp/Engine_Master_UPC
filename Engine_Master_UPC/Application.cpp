644e627d264d88bbc55375853f18477cdd00586c
Héctor Núñez Carpio <accounts.hector@nunezcarpio.com>
2026-05-12 20:14:11 +0200

    Music System

--- Engine_Master_UPC/Application.cpp
+++ Engine_Master_UPC/Application.cpp
@@ -17,6 +17,7 @@
 #include "ModuleNavigation.h"
 #include "ModuleTime.h"
 #include "ModuleHaptics.h"
+#include "ModuleMusic.h"
 
 #include "ScriptFactory.h"
 
@@ -56,6 +57,11 @@ Application::Application(int argc, wchar_t** argv, void* hWnd)
     modules.push_back(m_moduleCamera = new ModuleCamera());
     modules.push_back(m_moduleScene = new ModuleScene());
 
+    modules.push_back(m_moduleTrigger = new ModuleTrigger());
+
+    modules.push_back(m_moduleMusic = new ModuleMusic());
+
+    m_settings = new Settings();
 }
 
 Application::~Application()
