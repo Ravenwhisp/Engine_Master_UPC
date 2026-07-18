0f52370b1ad51abda1e046ca59be5c61950edfd9
Miquel Barbarà <miquel.barbara.garrido@gmail.com>
2026-07-18 18:40:46 +0200

    The AssetReference<DataContainer> was pointing to garbage

--- Engine_Master_UPC/DataContainer.cpp
+++ Engine_Master_UPC/DataContainer.cpp
@@ -4,18 +4,10 @@
 #include "ModuleAssets.h"
 #include "FieldUtils.h"
 #include "JsonArchive.h"
-#include "GenericTypeFactory.h"
-
 #include <string>
 
 void DataContainer::serialize(IArchive& archive)
 {
-	if (m_upgraded)
-	{
-		m_upgraded->serialize(archive);
-		return;
-	}
-
 	if (archive.mode() == ArchiveMode::Output && m_typeName.empty())
 		m_typeName = getTypeName();
 
@@ -26,25 +18,10 @@ void DataContainer::serialize(IArchive& archive)
 	{
 		FieldUtils::serialize(*this, reinterpret_cast<const char*>(this), archive);
 	}
-	else if (archive.mode() == ArchiveMode::Input && !m_typeName.empty() && DataContainerFactory::isRegistered(m_typeName))
-	{
-		AssetId ref = getReference();
-		m_upgraded = DataContainerFactory::create(m_typeName, ref);
-		if (m_upgraded)
-		{
-			FieldUtils::serialize(*m_upgraded, reinterpret_cast<const char*>(m_upgraded.get()), archive);
-		}
-	}
 }
 
 void DataContainer::drawUI()
 {
-	if (m_upgraded)
-	{
-		m_upgraded->drawUI();
-		return;
-	}
-
 	ImGui::Text("Data Container");
 	ImGui::Separator();
 
