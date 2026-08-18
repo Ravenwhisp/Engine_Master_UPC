fa4dbffbd50fff2258e1a98a792eff3380bf29b2
Jordi <pimoreno.j@outlook.es>
2026-08-18 17:19:50 +0200

    correcting GameRelease crash from particle update

--- Engine_Master_UPC/ModuleRender.h
+++ Engine_Master_UPC/ModuleRender.h
@@ -121,7 +121,7 @@ public:
     void markDebugDrawCacheDirty();
 
 private:
-    void initViewportGBuffers(RenderSurface& surface, float width, float height);
+    void initSceneRenderTargets(RenderSurface& surface, float width, float height);
 
     // Surface helpers
     void renderScene(ID3D12GraphicsCommandList4* commandList,const RenderCamera& camera,RenderSurface& outputSurface,bool renderDebug,RenderViewType viewType);
